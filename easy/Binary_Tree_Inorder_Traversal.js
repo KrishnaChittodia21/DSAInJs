caadc8ec4b6b3ecdc94fc2ed7b35e3953b2a6423
kannu21 <krishnachittodia.10@gmail.com>
2023-11-27 15:06:54 +0530

    added more questions

---
+++ easy/Binary_Tree_Inorder_Traversal.js
@@ -0,0 +1,23 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  if (!root) return []
+  const left = inorderTraversal(root.left)
+  const right = inorderTraversal(root.right)
+  return [...left, root.val, ...right]
+}
+
+const root = new TreeNode(
+  1,
+  new TreeNode(2, new TreeNode(3, null), new TreeNode(4, null)),
+  new TreeNode(5, new TreeNode(6, null), new TreeNode(7, new TreeNode(8, null)))
+)
+console.log(inorderTraversal(root))
