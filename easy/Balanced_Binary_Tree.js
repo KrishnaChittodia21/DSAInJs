e797a7100f0a0b7f4a58cc1b05bea3becc098d52
kannu21 <krishnachittodia.10@gmail.com>
2023-12-07 01:38:13 +0530

    added more questions

--- easy/Balanced_Binary_Tree.js
+++ easy/Balanced_Binary_Tree.js
@@ -23,3 +23,32 @@ var isBalanced = function (root) {
   const right = isBalanced(root.right)
   return Math.abs(height(root.left) - height(root.right)) <= 1 && left && right
 }
+
+const heightIterative = (node) => {
+  const stk = [[node, 1]]
+  let max = 0
+  while (stk.length) {
+    const [root, height] = stk.pop()
+    if (!root) continue
+    if (height > max) max = height
+    if (root.left) stk.push([root.left, height + 1])
+    if (root.right) stk.push([root.right, height + 1])
+  }
+  return max
+}
+
+const isBalancedBinaryTreeIterative = (root) => {
+  if (!root) return true
+  const stk = [root]
+  while (stk.length) {
+    const root = stk.pop()
+    if (!root) continue
+    if (Math.abs(heightIterative(root.left) - heightIterative(root.right)) > 1)
+      return false
+    if (root.left) stk.push(root.left)
+    if (root.right) stk.push(root.right)
+  }
+  return true
+}
+
+console.log(isBalancedBinaryTreeIterative([3, 9, 20, null, null, 15, 7]))
