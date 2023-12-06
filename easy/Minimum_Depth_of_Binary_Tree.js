1c84f66742105a524c9059907074bae03d50aa35
kannu21 <krishnachittodia.10@gmail.com>
2023-12-07 02:16:58 +0530

    added more questions

--- easy/Minimum_Depth_of_Binary_Tree.js
+++ easy/Minimum_Depth_of_Binary_Tree.js
@@ -1,11 +1,9 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
+
 /**
  * @param {TreeNode} root
  * @return {number}
@@ -18,3 +16,21 @@ var minDepth = function (root) {
     ? left + right + 1
     : Math.min(left, right) + 1
 }
+
+const minDepthIterative = (root) => {
+  if (!root) return 0
+  const stk = [[root, 1]]
+  while (stk.length) {
+    const [root, depth] = stk.shift()
+    if (!root.left && !root.right) return depth
+    if (root.left) stk.push([root.left, depth + 1])
+    if (root.right) stk.push([root.right, depth + 1])
+  }
+}
+
+const root = new TreeNode(
+  3,
+  new TreeNode(9),
+  new TreeNode(20, new TreeNode(15), new TreeNode(7))
+)
+console.log(minDepthIterative(root))
