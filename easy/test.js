196d26e02f2aa8747d8e136ebf24c3c726966215
kannu21 <krishnachittodia.10@gmail.com>
2023-12-14 23:41:28 +0530

    added more questions

--- easy/test.js
+++ easy/test.js
@@ -1,31 +1,13 @@
-const checkArray = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false
-  for (let i = 0; i < arr1.length; i++) {
-    console.log(arr1[i], arr2[i])
-    if (arr1[i] !== arr2[i]) return false
+var containsNearbyDuplicate = function (nums, k) {
+  let map = {}
+  for (let i = 0; i < nums.length; i++) {
+    console.log(map, nums[i], i)
+    if (map.hasOwnProperty(nums[i])) {
+      return Math.abs(i - map[nums[i]]) <= k
+    }
+    map[nums[i]] = i
   }
-  return true
+  return false
 }
 
-var areSimilar = function (mat, k) {
-  const tempArr = JSON.parse(JSON.stringify(mat))
-  while (k > 0) {
-    for (let i = 0; i < mat.length; i++) {
-      const temp = tempArr[i].shift()
-      tempArr[i].push(temp)
-    }
-    k--
-  }
-  for (let i = 0; i < mat.length; i++) {
-    if (!checkArray(mat[i], tempArr[i])) return false
-  }
-  return true
-}
-const mat = [
-  [1, 2, 1, 2],
-  [5, 5, 5, 5],
-  [6, 3, 6, 3],
-]
-const mat2 = [[1, 2]]
-const k = 1
-console.log(areSimilar(mat2, k))
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1))
