9fce3429b16a4c168f9766b5db5be45f6aede326
kannu21 <krishnachittodia.10@gmail.com>
2023-12-16 19:26:04 +0530

    added more questions

--- easy/Move_Zeroes.js
+++ easy/Move_Zeroes.js
@@ -3,19 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  if (!nums.length || nums.length === 1) return nums
   let i = 0
   let j = 0
-  while (i < nums.length) {
-    if (nums[j] === 0 && nums[i] !== nums[j]) {
+  while (j < nums.length) {
+    if (nums[j] !== 0) {
       let temp = nums[j]
       nums[j] = nums[i]
       nums[i] = temp
-      j++
+      i++
     }
-    i++
+    j++
   }
   return nums
 }
-
-console.log(moveZeroes([0, 1, 0, 3, 12]))
+console.log(moveZeroes([1, 0, 1]))
