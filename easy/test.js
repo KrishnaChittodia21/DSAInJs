ea310e4d29002d9933a4eab6a783471571a4d4db
kannu21 <krishnachittodia.10@gmail.com>
2023-12-26 23:52:18 +0530

    added more questions

--- easy/test.js
+++ easy/test.js
@@ -1,25 +1,20 @@
 /**
- * @param {number} num
- * @param {number} k
+ * @param {string} s
  * @return {number}
  */
-var divisorSubstrings = function (num, k) {
+var countGoodSubstrings = function (s) {
   let curr = ''
-  let count = 0
-  const numsStr = num.toString()
-  for (let i = 0; i < numsStr.length; i++) {
-    curr += numsStr.charAt(i)
-    if (curr.length === k) {
-      const currNum = parseInt(curr)
-      if (currNum !== 0 && num % currNum === 0) count++
+  let count = ''
+  for (let i = 0; i < s.length; i++) {
+    curr += s[i]
+    if (curr.length === 3) {
+      if (new Set(curr).size === 3) {
+        count++
+      }
       curr = curr.slice(1)
     }
   }
   return count
 }
 
-console.log(divisorSubstrings(240, 2))
-console.log(divisorSubstrings(430043, 2))
-console.log(divisorSubstrings(30003, 3))
-console.log(divisorSubstrings(680, 2))
-console.log(divisorSubstrings(2, 1))
+console.log(countGoodSubstrings('aababcabc'))
