0edcaeb243ca25afa74377468bcee5cf027cf8f6
kannu21 <krishnachittodia.10@gmail.com>
2023-12-28 01:14:41 +0530

    added more questions

--- easy/Power_of_Two.js
+++ easy/Power_of_Two.js
@@ -3,14 +3,11 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function (n) {
-  if (n === 1) return true
-  if (n % 2 !== 0 || n < 1) return false
-  let i = 1
-  while (n !== 1) {
+  while (n > 1) {
     if (n % 2 !== 0) return false
     n = n / 2
   }
-  return true
+  return n === 1
 }
 
 console.log(isPowerOfTwo(0))
