942e14839232a8bf2b215d6444fa4ce91f5d145b
kannu21 <krishnachittodia.10@gmail.com>
2023-12-30 18:54:23 +0530

    added more questions

--- easy/Convert_Binary_Number_in_a_Linked_List_to_Integer.js
+++ easy/Convert_Binary_Number_in_a_Linked_List_to_Integer.js
@@ -17,6 +17,15 @@ var getDecimalValue = function (head) {
   return arr.reduce((prev, curr) => prev * 2 + curr, 0)
 }
 
+const getDecimalValue2 = (head) => {
+  let res = ''
+  while (head) {
+    res += head.val
+    head = head.next
+  }
+  return parseInt(res, 2)
+}
+
 const list = new ListNode(1, new ListNode(0, new ListNode(1)))
 
-console.log(getDecimalValue(list))
+console.log(getDecimalValue2(list))
