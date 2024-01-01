ddb058788d9d095b468d72ca00027930be9c0b2c
kannu21 <krishnachittodia.10@gmail.com>
2024-01-01 23:33:08 +0530

    added more questions

--- easy/test.js
+++ easy/test.js
@@ -1,20 +1,10 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maximumStrongPairXor = function (nums) {
-  let xor = 0
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (Math.abs(nums[i] - nums[j]) <= Math.min(nums[i], nums[j])) {
-        xor = Math.max(xor, nums[i] ^ nums[j])
-      }
-    }
-  }
-  return xor
+const arr = [
+  [1, 3],
+  [2, 2],
+]
+const flatArray = (arr) => {
+  return arr.reduce((acc, curr) => {
+    return acc.concat(Array?.isArray(curr) ? flatArray(curr) : curr)
+  }, [])
 }
-
-// console.log(maximumStrongPairXor([1, 2, 3, 4, 5]))
-// console.log(maximumStrongPairXor([10, 100]))
-// console.log(maximumStrongPairXor([5, 6, 25, 30]))
-console.log(maximumStrongPairXor([1, 5, 4, 1, 7]))
+console.log(flatArray(arr))
