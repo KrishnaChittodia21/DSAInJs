7042accae9a0d72a0f019421fe6a2deb916165dd
kannu21 <krishnachittodia.10@gmail.com>
2024-01-12 01:34:54 +0530

    added more questions

--- easy/test.js
+++ easy/test.js
@@ -1,11 +1,13 @@
 // const n = 1
 // console.log((n ^ 7) | 1)
 // console.log(1 >> 1)
-let count = 0
-let num = 10
-while (num > 0) {
-  count += num & 1
-  num >>= 1
-  console.log(num, count)
-}
-console.log(count)
+// let count = 0
+// let num = 10
+// while (num > 0) {
+//   count += num & 1
+//   num >>= 1
+//   console.log(num, count)
+// }
+// console.log(Number.parseInt('101'))
+const n = 0o1011
+console.log(n.toString().replaceAll('0', '').length)
