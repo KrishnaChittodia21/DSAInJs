551d0c5db06ae67797f67b53a4a505c369236204
kannu21 <krishnachittodia.10@gmail.com>
2024-01-21 00:37:14 +0530

    added more questions

--- easy/The_K_Weakest_Rows_in_a_Matrix.js
+++ easy/The_K_Weakest_Rows_in_a_Matrix.js
@@ -13,9 +13,13 @@ var kWeakestRows = function (mat, k) {
     })
   }
   solDierCount.sort((a, b) => a.value - b.value)
+  if (k === solDierCount.length) {
+    return solDierCount.map((x) => x.index)
+  }
   let i = 1
   const res = []
   while (k > 0) {
+    console.log(i)
     if (solDierCount[i - 1].value <= solDierCount[i].value) {
       res.push(solDierCount[i - 1].index)
     }
@@ -25,35 +29,38 @@ var kWeakestRows = function (mat, k) {
   return res
 }
 
-console.log(
-  kWeakestRows(
-    [
-      [1, 1, 0, 0, 0],
-      [1, 1, 1, 1, 0],
-      [1, 0, 0, 0, 0],
-      [1, 1, 0, 0, 0],
-      [1, 1, 1, 1, 1],
-    ],
-    3
-  )
-)
-console.log(
-  kWeakestRows(
-    [
-      [1, 0, 0, 0],
-      [1, 1, 1, 1],
-      [1, 0, 0, 0],
-      [1, 0, 0, 0],
-    ],
-    2
-  )
-)
+// console.log(
+//   kWeakestRows(
+//     [
+//       [1, 1, 0, 0, 0],
+//       [1, 1, 1, 1, 0],
+//       [1, 0, 0, 0, 0],
+//       [1, 1, 0, 0, 0],
+//       [1, 1, 1, 1, 1],
+//     ],
+//     3
+//   )
+// )
+// console.log(
+//   kWeakestRows(
+//     [
+//       [1, 0, 0, 0],
+//       [1, 1, 1, 1],
+//       [1, 0, 0, 0],
+//       [1, 0, 0, 0],
+//     ],
+//     2
+//   )
+// )
 
 console.log(
-  kWeakestRows([
-    [1, 0],
-    [1, 0],
-    [1, 0],
-    [1, 1],
-  ])
+  kWeakestRows(
+    [
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 1],
+    ],
+    4
+  )
 )
