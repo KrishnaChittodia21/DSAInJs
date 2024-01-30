9847a9adf26f3bf56d685562917ee8ba7b196d5a
KrishnaChittodia21 <krishnachittodia.10@gmail.com>
2024-01-31 01:58:00 +0530

    added more questions

---
+++ easy/Neither_Minimum_nor_Maximum.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNonMinOrMax = function (nums) {
+  if (nums.length < 3) return -1
+  nums.sort((a, b) => a - b)
+  return nums[Math.floor(nums.length / 2)]
+}
