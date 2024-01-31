c1ccc59ac107d1ea2a6e27f96964067fd3b04975
KrishnaChittodia21 <krishnachittodia.10@gmail.com>
2024-02-01 00:18:02 +0530

    added more questions

--- easy/Smallest_Missing_Integer_Greater_Than_Sequential_Prefix_Sum.js
+++ easy/Smallest_Missing_Integer_Greater_Than_Sequential_Prefix_Sum.js
@@ -2,4 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingInteger = function (nums) {}
+var missingInteger = function (nums) {
+  let e = 1
+  let sum = nums[0]
+  while (nums[e] === nums[e - 1] + 1) {
+    sum += nums[e - 1]
+    e++
+  }
+  while (nums.includes(sum)) {
+    sum++
+  }
+  return sum
+}
