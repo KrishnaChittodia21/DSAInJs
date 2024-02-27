0b4761edeeedea10b086794058ffe42efd9fbe58
KrishnaChittodia21 <krishnachittodia.10@gmail.com>
2024-02-28 02:39:13 +0530

    added more questions

--- javascript/Flatten_Deeply_Nested_Array.js
+++ javascript/Flatten_Deeply_Nested_Array.js
@@ -3,4 +3,14 @@
  * @param {number} depth
  * @return {Array}
  */
-var flat = function (arr, n) {}
+var flat = function (arr, n) {
+  if (n === 0) {
+    return arr
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flat(arr[i], n - 1)
+    }
+  }
+  return arr
+}
